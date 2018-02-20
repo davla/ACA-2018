8a10db3d2d74d1dea7de6a48ef6b0f4db0036f30
wen2720 <wenhao2720@gmail.com>
2018-02-20 13:50:03 +0100

    ex-Uart

--- examples/src/main/scala/uart/Uart.scala
+++ examples/src/main/scala/uart/Uart.scala
@@ -2,9 +2,9 @@
  * Copyright: 2014-2018, Technical University of Denmark, DTU Compute
  * Author: Martin Schoeberl (martin@jopdesign.com)
  * License: Simplified BSD License
- *
+ * 
  * A UART is a serial port, also called an RS232 interface.
- *
+ * 
  */
 
 package uart
@@ -117,84 +117,156 @@ class Rx(frequency: Int, baudRate: Int) extends Module {
 /**
  * A single byte buffer with an AXI style channel
  */
+//# Buffer class - component
 class Buffer extends Module {
   val io = new Bundle {
-    val in = new Channel()
-    val out = new Channel().flip
+    val in = new Channel()	//#declare in assigned with new channel obj
+    val out = new Channel().flip //#declare out assigned with new channel obj fliped 
   }
 
-  val empty :: full :: Nil = Enum(UInt(), 2)
-  val stateReg = Reg(init = empty)
-  val dataReg = Reg(init = Bits(0, 8))
+  val empty :: full :: Nil = Enum(UInt(), 2) //# declare states
+  val stateReg = Reg(init = empty)			 //# declare state register and initialize to empty
+  val dataReg = Reg(init = Bits(0, 8))		 //# declare data register 
 
-  io.in.ready := stateReg === empty
-  io.out.valid := stateReg === full
+  io.in.ready := stateReg === empty			 //# assign io.in.ready with true when stateReg === empty, tell buffered transmitter data is ready 
+  io.out.valid := stateReg === full			 //# assign io.out.valid with true when stateReg === full, tell transmitter to shift data
 
-  when(stateReg === empty) {
-    when(io.in.valid) {
-      dataReg := io.in.data
-      stateReg := full
+  when(stateReg === empty) {				 //# when state is empty
+    when(io.in.valid) {						 //# if io.in.valid is true then 
+      dataReg := io.in.data					 //# assign input data to dataReg
+      stateReg := full						 //# next state is changed to full
     }
-  }.otherwise { // full, io.out.valid := true
-    when(io.out.ready) {
-      stateReg := empty
+  }.otherwise { // full, io.out.valid := true //# when state is not empty
+    when(io.out.ready) {					  //# if io.out.ready is true then
+      stateReg := empty						  //# next state is change to empty 
     }
   }
-  io.out.data := dataReg
+  io.out.data := dataReg					  //# output io.out.data is assigned by dataReg
 }
 
 /**
  * A transmitter with a single buffer.
  */
+//# BufferedTx class - component
 class BufferedTx(frequency: Int, baudRate: Int) extends Module {
   val io = new Bundle {
-    val txd = Bits(OUTPUT, 1)
-    val channel = new Channel()
+    val txd = Bits(OUTPUT, 1)	//#declare transmitt data txd as output 
+    val channel = new Channel() //#assign new Channel obj to channel
   }
-  val tx = Module(new Tx(frequency, baudRate))
-  val buf = Module(new Buffer())
+  val tx = Module(new Tx(frequency, baudRate)) //#declare tx assign with new Tx obj
+  val buf = Module(new Buffer())			   //#declare buf assign with new Buffer obj
 
-  buf.io.in <> io.channel
-  tx.io.channel <> buf.io.out
-  io.txd <> tx.io.txd
+  buf.io.in <> io.channel					   //#connect BufferedTx io.channel to buffer io in(channel obj), data in, valid(full state) flag in, ready(empty state)flag out
+  tx.io.channel <> buf.io.out				   //#connect buf.io.out(channel obj), data out, valid(full state) flag out, ready(empty state)flag in to tx.io.channel
+  io.txd <> tx.io.txd						   //#connect tx.io.txd to BufferedTx io txd
 }
 
 /**
  * Send 'hello'.
  */
-class Sender(frequency: Int, baudRate: Int) extends Module {
+//# Sender class - component
+class Sender1(frequency: Int, baudRate: Int) extends Module {
   val io = new Bundle {
-    val txd = Bits(OUTPUT, 1)
-    val led = Bits(OUTPUT, 1)
+    val txd = Bits(OUTPUT, 1) //# declare transmitt txd as output
+	val led = Bits(OUTPUT, 1)
   }
 
-  val tx = Module(new BufferedTx(frequency, baudRate))
+  val tx = Module(new BufferedTx(frequency, baudRate)) //#assign tx with new BuuferedTx object
 
-  io.txd := tx.io.txd
+  io.txd := tx.io.txd								   //#assign io.txd with tx.io.txd, bypass
 
   // This is not super elegant
-  // val hello = Array.fill(5)(Bits('1'))
-  val hello = Array.tabulate[Bits](10)(n => Bits(if (n % 2 == 0) '0' else '1'))
-    .flatMap(b => Array.fill(100)(b))
-  // val hello = Array.tabulate[Bits](10)(n => Bits((n % 2).toChar)
-  // val hello = Array[Bits](Bits('H'), Bits('e'), Bits('l'), Bits('l'), Bits('o'))
-  val text = Vec[Bits](hello)
+//  val hello = Array[Bits](Bits('H'), Bits('e'), Bits('l'), Bits('l'), Bits('o'))
+//  val text = Vec[Bits](hello)
 
-  val cntReg = Reg(init = UInt(0, log2Up(hello.length)))
-  val ledOut = Reg(init = UInt(0, 1))
+//  val cntReg = Reg(init = UInt(0, 3))
 
-  tx.io.channel.data := text(cntReg)
-  tx.io.channel.valid := cntReg =/= UInt(hello.length)
-  io.led := ledOut
+//  tx.io.channel.data := text(cntReg)
+//  tx.io.channel.valid := cntReg =/= UInt(5)
 
-  when(tx.io.channel.ready && cntReg =/= UInt(hello.length)) {
-        cntReg := cntReg + UInt(1)
-        when (text(cntReg) === Bits('0')) {
-            ledOut := UInt(0, 1)
-        } otherwise {
-            ledOut := UInt(1, 1)
-        }
+//  when(tx.io.channel.ready && cntReg =/= UInt(5)) {
+//    cntReg := cntReg + UInt(1)
+//  }					
+  val cmdReg = Reg(init = UInt(0, 8))
+  val validReg = Reg(init = UInt(0, 1))
+  tx.io.channel.data := cmdReg		
+  tx.io.channel.valid := validReg =/= UInt(1)
+  val cntReg = Reg(init = UInt(frequency / 2 - 1, 32))
+  val blkReg = Reg(init = UInt(0, 1))
+//  cntReg := cntReg - UInt(1)					--bad example
+//  when(cntReg === UInt(0)) {
+//    cntReg := UInt(frequency / 2 - 1)
+//    blkReg := ~blkReg
+//    when(blkReg === UInt(0)){
+//      cmdReg := UInt("h30")
+//    }.elsewhen(blkReg === UInt(1)){
+//	  cmdReg := UInt("h31")
+//    }
+//	when(tx.io.channel.ready) { 
+//      validReg := UInt(1)	
+//    }
+//  }
+  when(cntReg =/= UInt(0)) {
+    cntReg := cntReg - UInt(1)
+	validReg := UInt(1)
+  }.elsewhen(cntReg === UInt(0)){
+    cntReg := UInt(frequency / 2 - 1)
+    blkReg := ~blkReg
+    when(blkReg === UInt(0)){
+      cmdReg := UInt("h30")
+    }.elsewhen(blkReg === UInt(1)){
+	  cmdReg := UInt("h31")
     }
+	when(tx.io.channel.ready) { 
+      validReg := UInt(0)	
+    }
+  }
+  io.led := blkReg
+}
+
+class Sender2(frequency: Int, baudRate: Int) extends Module {
+  val io = new Bundle {
+    val txd = Bits(OUTPUT, 1) //# declare transmitt txd as output
+	val led = Bits(OUTPUT, 1)
+  }
+
+  val tx = Module(new BufferedTx(frequency, baudRate)) //#assign tx with new BuuferedTx object
+
+  io.txd := tx.io.txd								   //#assign io.txd with tx.io.txd, bypass
+
+  // This is not super elegant
+  val number = Array[Bits](Bits('9'), Bits('8'), Bits('7'), Bits('6'), Bits('5'), Bits('4'), Bits('3'), Bits('2'), Bits('1'), Bits('0'))
+  val text = Vec[Bits](number)
+  val validReg = Reg(init = UInt(0, 1))
+  val cntReg = Reg(init = UInt(10, 4))
+  val blkReg = Reg(init = UInt(0, 1))
+  tx.io.channel.data := text(cntReg)
+  tx.io.channel.valid := validReg =/= UInt(1)
+  when(tx.io.channel.ready && cntReg =/= UInt(0)){
+	cntReg := cntReg - UInt(1)
+    validReg := UInt(0)
+  }.elsewhen(tx.io.channel.ready && cntReg === UInt(0)){
+    validReg := UInt(1)
+	cntReg := UInt(10)
+  }
+  io.led := blkReg
+}
+
+class Led(frequency: Int) extends Module {
+  val io = new Bundle {
+    val led = UInt(OUTPUT, 1)
+  }
+  val CNT_MAX = UInt(frequency / 2 - 1);
+  
+  val cntReg = Reg(init = UInt(0, 32))
+  val blkReg = Reg(init = UInt(0, 1))
+
+  cntReg := cntReg + UInt(1)
+  when(cntReg === CNT_MAX) {
+    cntReg := UInt(0)
+    blkReg := ~blkReg
+  }
+  io.led := blkReg
 }
 
 class Echo(frequency: Int, baudRate: Int) extends Module {
@@ -208,27 +280,31 @@ class Echo(frequency: Int, baudRate: Int) extends Module {
   io.txd := tx.io.txd
   rx.io.rxd := io.rxd
   tx.io.channel <> rx.io.channel
-  tx.io.channel.valid := Bool(true)
+  tx.io.channel.valid := Bool(true)	
 //  tx.io.channel.data := Bits('H')
 }
-
+//# UartMain class - component
 class UartMain(frequency: Int, baudRate: Int) extends Module {
   val io = new Bundle {
-    val rxd = Bits(INPUT, 1)
-    val txd = Bits(OUTPUT, 1)
-    val led = Bits(OUTPUT, 10)
+    val rxd = Bits(INPUT, 1)	//# declare recieve data rxd as input
+    val txd = Bits(OUTPUT, 1)	//# declare transmitt data txd as output
+	val ledG = Bits(OUTPUT, 1)
+    val ledR = Bits(OUTPUT, 1)
   }
-
-  val u = Module(new Sender(frequency, baudRate))
+  
+  val u = Module(new Sender2(50000000, 115200))	//# assign u with new Sender obj with argument
   // val u = Module(new Echo(50000000, 115200))
-  io.txd := u.io.txd
-  io.led := Cat(Bits(0, 9), u.io.led)
+  io.txd := u.io.txd							//# assign UartMain component io.txd with sender io txd, bypassing txd from sender
   // u.io.rxd := io.rxd
+  val l = Module(new Led(frequency))
+  io.ledG := l.io.led
+  io.ledR := u.io.led
 }
-
+//#chisel main
 object UartMain {
   def main(args: Array[String]): Unit = {
     chiselMain(Array[String]("--backend", "v", "--targetDir", "generated"),
-      () => Module(new UartMain(50000000, 115200)))
+      () => Module(new UartMain(50000000, 115200))) //#pass argument to new object UartMain
   }
 }
+
