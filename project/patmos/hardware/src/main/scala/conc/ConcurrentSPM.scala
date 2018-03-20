a5df177f2075571120849324c87db05aca94f292
Davide Laezza <truzzialrogo@gmx.com>
2018-03-20 16:36:33 +0100

    Refactoring + fixing of ConcurrentSPM

--- project/patmos/hardware/src/main/scala/conc/ConcurrentSPM.scala
+++ project/patmos/hardware/src/main/scala/conc/ConcurrentSPM.scala
@@ -17,7 +17,7 @@ class ConcurrentSPM (
     val granularity :Int,
     nrCores: Int,
     size: Int
-) extends SharedSPM(nrCores, size) {
+) extends Module {
     import ConcurrentSPM._
 
     if (!isPow2 (granularity)) {
@@ -26,32 +26,59 @@ class ConcurrentSPM (
 
     // Check for other exceptions
 
+    val io = Vec(nrCores, new OcpCoreSlavePort(ADDR_WIDTH, DATA_WIDTH))
+    val nd = createArbiters (nrCores)
+    val spm = Module(new Spm(size))
+    wireSPM (spm)
+
     val statusBits = Bits (0, width=(size / granularity))
 
     val thisStatusBit = getStatusBit (spm.io.M.Addr)
-    val cmd = nd.map(_.io.toMem.M.Cmd).reduce((x, y) => x | y)
 
-    spm.io.M.Cmd := OcpCmd.IDLE
-
-    switch (cmd) {
+    switch (nd.map(_.io.toMem.M.Cmd).reduce((x, y) => x | y)) {
 
         // Every read is a Load Linked
         is (OcpCmd.RD) {
             thisStatusBit := StatusBit.PRISTINE
+            // Resp is set to DVA by spm since we set CMD to RD
+            spm.io.M.Cmd := OcpCmd.RD
         }
 
         // Every write is a Store Conditional
         is (OcpCmd.WR) {
-            // We need to write success/failure as output
             when (thisStatusBit === StatusBit.PRISTINE) {
                 thisStatusBit := StatusBit.DIRTY
-                spm.io.M.Cmd := cmd
+                spm.io.M.Cmd := OcpCmd.WR
+                // Resp is set to DVA by spm since we set CMD to WR
+                spm.io.S.Data := StatusCode.SUCCESS
+            }.otherwise {
+                spm.io.S.Resp := OcpResp.DVA
+                spm.io.S.Data := StatusCode.FAILURE
             }
         }
     }
 
+    def createArbiters(nrCores :Int) = {
+        val nd = new Array[NodeSPM](nrCores)
+        for (i <- 0 until nrCores) {
+          nd(i) = Module(new NodeSPM(i, nrCores))
+          nd(i).io.fromCore <> io(i)
+          nd(i).io.toMem.S <> spm.io.S
+        }
+        nd
+    }
+
     def getStatusBit (address: UInt) = {
          statusBits (address >> log2Up (granularity))
+    }
+
+    def wireSPM(spm :Spm) = {
+        // Or the master signals
+        spm.io.M.Addr := nd.map(_.io.toMem.M.Addr).reduce((x, y) => x | y)
+        spm.io.M.Data := nd.map(_.io.toMem.M.Data).reduce((x, y) => x | y)
+        spm.io.M.ByteEn := nd.map(_.io.toMem.M.ByteEn).reduce((x, y) => x | y)
+
+        spm.io.M.Cmd := OcpCmd.IDLE
     }
 }
 
@@ -59,6 +86,11 @@ object ConcurrentSPM {
     object StatusBit {
         val PRISTINE = Bits(0, width=1)
         val DIRTY = Bits(1, width=1)
+    }
+
+    object StatusCode {
+        val SUCCESS = UInt(0)
+        val FAILURE = UInt(1)
     }
 
     def apply (granularity :Int, nrCores :Int, size :Int) =
