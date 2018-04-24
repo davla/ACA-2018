e2d25e5427c309912477d159d05281a7a4a7a033
Davide Laezza <truzzialrogo@gmx.com>
2018-04-24 17:08:20 +0200

    Missing a way to check last write

--- project/patmos/hardware/src/main/scala/conc/TransSpm.scala
+++ project/patmos/hardware/src/main/scala/conc/TransSpm.scala
@@ -14,7 +14,7 @@ import patmos._
 import patmos.Constants._
 
 object StatusBits {
-    val WRITTEN_BEFORE_START = Bits("b00")
+    val WRITTEN_BEFORE_START = Bits("b00")  //initial state
     val UNTOUCHED_SINCE_START = Bits("b01")
     val WRITTEN_AFTER_START = Bits("b11")
     val WRITTEN_IN_COMMIT = Bits("b10")
@@ -38,15 +38,38 @@ object StatusBits {
         applyBitMask(location, andBitMask, orBitMask)
     }
 
-    def noInCommit(location :Bits) = {
+    def makeWritten(location :Bits, index :UInt) = {
+        val andBitMask = ~ (UInt(0, width = stateWidth) << (index << indexShift))
+        val orBitMask = UInt(0, width = location.getWidth)
+        applyBitMask(location, andBitMask, orBitMask)
+    }
+
+    def hasUncommitted(location :Bits, commitStatus :Bits) = {
         val nrCores = location.getWidth / stateWidth
-        var found = Bool(false)
+        val found = Bits()
+        found := Bits(0, width = nrCores)
+        found.setWidth(nrCores)
+
         for (k <- 0 until nrCores) {
-            when (get(location, k) === StatusBits.WRITTEN_IN_COMMIT) {
-                found = Bool(true)
-            }
+            val coreState = get(location, k)
+            found(k) := commitStatus(k) && coreState === UNTOUCHED_SINCE_START
         }
-        found
+
+        found.orR()
+    }
+
+    def hasInCommit(location :Bits) = {
+        val nrCores = location.getWidth / stateWidth
+        var found = Bits()
+        found := Bits(0, width = nrCores)
+        found.setWidth(nrCores)
+
+        for (k <- 0 until nrCores) {
+            val coreState = get(location, k)
+            found(k) := coreState === WRITTEN_IN_COMMIT
+        }
+
+        found.orR()
     }
 
     def write(location :Bits, core :UInt) = {
@@ -59,7 +82,30 @@ object StatusBits {
 
             // Current core should write WRITTEN_IN_COMMIT
             when (UInt(k) === core) {
-                newLocation(end, start) := StatusBits.WRITTEN_IN_COMMIT
+                newLocation(end, start) := WRITTEN_IN_COMMIT
+            }
+
+            // Core has read, setting to WRITTEN_AFTER_START
+            .elsewhen (get(location, k) === StatusBits.UNTOUCHED_SINCE_START) {
+                newLocation(end, start) := StatusBits.WRITTEN_AFTER_START
+            }
+        }
+
+        newLocation
+    }
+
+    def write0(location :Bits, core :UInt, doneCommitting :Bool) = {
+        val nrCores = location.getWidth / stateWidth
+        val newLocation = location
+
+        for (k <- 0 until nrCores) {
+            val start = k * stateWidth
+            val end = start + stateWidth - 1
+
+            // Current core should write WRITTEN_IN_COMMIT
+            when (UInt(k) === core) {
+                newLocation(end, start) := Mux(doneCommitting,
+                    WRITTEN_BEFORE_START, WRITTEN_IN_COMMIT)
             }
 
             // Core has read, setting to WRITTEN_AFTER_START
@@ -98,10 +144,12 @@ class TransSpm(
     val io = new Bundle() {
         val slave = new OcpCoreSlavePort(ADDR_WIDTH, DATA_WIDTH)
         val core = UInt(INPUT, log2Up(nrCores))
-        val db = Bits(OUTPUT, 8)
+        val location = Bits(OUTPUT, 8)
+        val db = Bits(OUTPUT, 2)
     }
 
     val addrBits = log2Up(size / BYTES_PER_WORD)
+    val statusBitsSize = size / granularity
 
     // generate byte memories
     val mem = new Array[MemBlockIO](BYTES_PER_WORD)
@@ -110,29 +158,41 @@ class TransSpm(
     }
 
     // Status bits
-    // val statusBits = Vec.fill(size / granularity)(RegInit(StatusBits(nrCores)))
-    val statusBits = Mem(StatusBits(nrCores), size / granularity)
+    val statusBits = Vec.fill(statusBitsSize)(RegInit(StatusBits(nrCores)))
+    // val statusBits = Mem(StatusBits(nrCores), statusBitsSize)
 
     // Or gates
     val allUntouched = Vec.tabulate (nrCores) (core => {
-        (0 until size / granularity)
-            .map(addr => StatusBits.get(statusBits(UInt(addr)), core)(1))
-            .reduce(_ | _) === Bits(0)
+        val allLeftBits = statusBits.map(location => {
+            val coreState = StatusBits.get(location, core)
+            coreState(1)
+        })
+        orAll(allLeftBits.toArray) === Bits(0)
     })
-    // val allUntouched = Vec(statusBits.map(location => {
-    //         println(location.getWidth)
-    //         Array.tabulate(nrCores) (core =>
-    //             location(core * StatusBits.stateWidth + 1))
-    //     }).reduceLeft((acc, bits) =>
-    //         acc.zip(bits).map(pair => pair._1 | pair._2)
-    //     ))
+    val allWritten = Vec.tabulate (nrCores) (core => {
+        val allRightBits = statusBits.map(location => {
+            val coreState = StatusBits.get(location, core)
+            coreState(0)
+        })
+        orAll(allRightBits.toArray) === Bits(0)
+    })
+
+    // Commit bits
+    val inCommit = RegInit(Bits(0, width = nrCores))
 
     val currentStatusBits = getStatusBits(io.slave.M.Addr)
-    val canWrite = allUntouched(io.core) && StatusBits.noInCommit(currentStatusBits)
+
+    val hasUncommitted = StatusBits.hasUncommitted(currentStatusBits, inCommit)
+    val hasInCommit = StatusBits.hasInCommit(currentStatusBits)
+    val committingHere = hasUncommitted || hasInCommit
+
+    val canRead = ~hasUncommitted
+    val canWrite = ~committingHere && (inCommit(io.core) || allUntouched(io.core))
+
     val shouldWrite = io.slave.M.Cmd === OcpCmd.WR && canWrite
 
-    io.db := allUntouched(io.core)
-    // io.db := StatusBits.write(currentStatusBits, io.core)
+    io.db := hasUncommitted ## hasInCommit
+    io.location := currentStatusBits
 
     val cmdReg = Reg(next = io.slave.M.Cmd)
     val dataReg = RegInit(io.slave.M.Data)
@@ -144,7 +204,7 @@ class TransSpm(
             io.slave.M.Data(BYTE_WIDTH*(i+1)-1, BYTE_WIDTH*i))
     }
 
-    // COncatenates output from the memory blocks
+    // Concatenates output from the memory blocks
     val rdData = mem.map(_(io.slave.M.Addr(addrBits + 1, 2))).reduceLeft((x,y) => y ## x)
 
     io.slave.S.Resp := Mux(cmdReg === OcpCmd.WR || cmdReg === OcpCmd.RD,
@@ -153,13 +213,21 @@ class TransSpm(
 
     switch (io.slave.M.Cmd) {
         is (OcpCmd.RD) {
-            currentStatusBits := StatusBits.makeUntouched(currentStatusBits, io.core)
+            when (canRead) {
+                currentStatusBits := StatusBits.makeUntouched(currentStatusBits, io.core)
+            }
         }
 
         is (OcpCmd.WR) {
             when (canWrite) {
-                currentStatusBits := StatusBits.write(currentStatusBits, io.core)
+                currentStatusBits := StatusBits.write(currentStatusBits,
+                        io.core)
                 dataReg := Bits(0, width = DATA_WIDTH)
+
+                inCommit(io.core) := Mux(allWritten(io.core), Bool(false), Bool(true))
+                when (allWritten(io.core)) {
+                    doCommit(io.core, io.slave.M.Addr)
+                }
             }.otherwise {
                 dataReg := Bits(1, width = DATA_WIDTH)
             }
@@ -170,6 +238,25 @@ class TransSpm(
         val statusBitsAddr = address >> log2Up(granularity)
         statusBits(statusBitsAddr)
     }
+
+    def doCommit(core :UInt, currAddr :UInt) = {
+        for (addr <- 0 until statusBitsSize) {
+            val location = statusBits(UInt(addr))
+            location := Mux(UInt(addr) === currAddr,
+                StatusBits.write(currentStatusBits, io.core),
+                StatusBits.makeWritten(location, core))
+        }
+    }
+
+    // val collectCoresState = Vec.tabulate (nrCores) (core => {
+    //     val allRightBits = (0 until statusBitsSize).map(UInt(_))
+    //         .map(addr => {
+    //             val statusBits = statusBits(addr)
+    //             val coreState = StatusBits.get(statusBits, core)
+    //             coreState(1)
+    //         })
+    //     orAll(allLeftBits) === Bits(0)
+    // })
 }
 
 object TransSpm {
@@ -186,12 +273,15 @@ class TransSpmTester(dut: TransSpm) extends Tester(dut) {
   def read(addr: Int) = {
     poke(dut.io.slave.M.Addr, addr)
     poke(dut.io.slave.M.Cmd, 2) // OcpCmd.RD
+    peek(dut.io.db)
+    peek(dut.io.location)
     step(1)
     poke(dut.io.slave.M.Cmd, 0) // OcpCmd.IDLE
     while (peek(dut.io.slave.S.Resp) != 1) {
       step(1)
     }
     peek(dut.io.db)
+    peek(dut.io.location)
     peek(dut.io.slave.S.Data)
     dut.io.slave.S.Data
   }
@@ -201,12 +291,15 @@ class TransSpmTester(dut: TransSpm) extends Tester(dut) {
     poke(dut.io.slave.M.Data, data)
     poke(dut.io.slave.M.Cmd, 1) // OcpCmd.WR
     poke(dut.io.slave.M.ByteEn, 0x0f)
+    peek(dut.io.db)
+    peek(dut.io.location)
     step(1)
     poke(dut.io.slave.M.Cmd, 0) // OcpCmd.IDLE
     while (peek(dut.io.slave.S.Resp) != 1) {
       step(1)
     }
     peek(dut.io.db)
+    peek(dut.io.location)
     peek(dut.io.slave.S.Data)
     dut.io.slave.S.Data
   }
@@ -214,7 +307,9 @@ class TransSpmTester(dut: TransSpm) extends Tester(dut) {
   poke(dut.io.core, 0)
 
   // Basic read-write test
-
+  // for (i <- 0.until(1024, GRANULARITY)) {
+  //   read(i)
+  // }
   for (i <- 0.until(1024, GRANULARITY)) {
     expect(write(i, i * 0x100 + 0xa), 0)
   }
@@ -228,8 +323,8 @@ class TransSpmTester(dut: TransSpm) extends Tester(dut) {
   poke(dut.io.core, 0)
   read(0)
 
-  // poke(dut.io.core, 1)
-  // expect(write(0, 0xFF), 0)
+  poke(dut.io.core, 1)
+  expect(write(0, 0xFF), 0)
 
   poke(dut.io.core, 0)
   expect(write(0, 0xFF), 1)
