2a8dd5baeca19da82781434aec2e885a8c6f57bd
Davide Laezza <truzzialrogo@gmx.com>
2018-04-27 12:49:36 +0200

    Only failed read test missing

--- project/patmos/hardware/src/main/scala/conc/TransSpm.scala
+++ project/patmos/hardware/src/main/scala/conc/TransSpm.scala
@@ -13,97 +13,26 @@ import ocp._
 import patmos._
 import patmos.Constants._
 
-class StatusBits (
-    nrCores :Int,
-    size :Int
-) extends Module {
-    import StatusBits._
+object StatusBits {
+    val WRITTEN_BEFORE_START = Bits("b00")  //initial state
+    val UNTOUCHED_SINCE_START = Bits("b01")
+    val WRITTEN_AFTER_START = Bits("b11")
+    val WRITTEN_IN_COMMIT = Bits("b10")
 
-    val io = new Bundle() {
-        val addr = Bits(INPUT, log2Up(size))
-        val core = Bits(INPUT, log2Up(nrCores))
-        val dva = Bool(INPUT)
-        val data = Bits(INPUT, stateWidth)
-        val canRead = Bits(OUTPUT, 1)
-        val canWrite = Bits(OUTPUT, 1)
-        val allWritten = Bits(OUTPUT, 1)
+    val stateWidth = WRITTEN_BEFORE_START.getWidth
+    val indexShift = log2Up(stateWidth)
+
+    def apply(nrCores :Int) = {
+        val location = Bits()
+        location := Fill(nrCores, WRITTEN_BEFORE_START)
+        location
     }
 
-    // Status bits
-    val statusBits = Vec.fill(size)(RegInit(Fill(nrCores, WRITTEN_BEFORE_START)))
-
-    // Each of the OR gates here has as input the left bit of a core state
-    // for every location. It is used to check whether any location is in the
-    // WRITTEN_AFTER_START state. It is ignored during commits, that is the
-    // only moment in which the state can be WRITTEN_IN_COMMIT
-    val allUntouchedArr = new Array[Bool](nrCores)
-    for (core <- 0 until nrCores) {
-        val leftBits = new Array[Bits](statusBitsSize)
-        for (addr <- 0 until statusBitsSize) {
-            val location = statusBits(addr)
-            val coreState = StatusBits.get(location, core)
-            leftBits(addr) = coreState(1)
-        }
-        allUntouchedArr(core) = orAll(leftBits) === Bits(0)
-    }
-    val allUntouched = Vec(allUntouchedArr)
-
-    // Each of these bit-strings is the concatenation of the right bit of a
-    // core state for every location. As explained below, they are used to
-    // detect the end of a commit
-    val allRightBitsArr = new Array[Bits](nrCores)
-    for (core <- 0 until nrCores) {
-        val rightBits = new Array[Bits](statusBitsSize)
-        for (addr <- 0 until statusBitsSize) {
-            val location = statusBits(addr)
-            val coreState = StatusBits.get(location, core)
-            rightBits(addr) = coreState(0)
-        }
-        allRightBitsArr(core) = catAll(rightBits)
-    }
-    val allRightBits = Vec(allRightBitsArr)
-
-    // After masking the bit for the current location, the negation of the
-    // recursive OR of the bit strings for this core tells whether all the
-    // read locations have been written to, marking the end of a commit. The
-    // masking of the current location assumes a write in that location in the
-    // current cycle.
-    io.allWritten := ~((allRightBits(io.core) & ~(UInt(1) << io.addr)).orR)
-
-    val location = statusBits(io.addr)
-
-    val hasUncommitted = StatusBits.orMap(location, isUncommitted _)
-    val hasInCommit = StatusBits.orMap(location, isInCommit _)
-    val committingHere = hasUncommitted || hasInCommit
-
-    io.canRead := ~hasUncommitted
-    io.canWrite := ~committingHere && (inCommit(io.core) || allUntouched(io.core))
-
-    when (io.dva) {
-        switch (io.data) {
-            is (UNTOUCHED_SINCE_START) {
-                makeUntouched()
-            }
-
-            is (WRITTEN_IN_COMMIT) {
-                for (addr <- 0 until size) {
-                    val thisLocation = statusBits(size)
-
-                    when (UInt(addr) === io.addr) {
-                        thisLocation := write(thisLocation, io.core, Bool(false))
-
-                    }.otherwise {
-                        makeWrittenBefore(thisLocation, io.core)
-                    }
-            }
-        }
-    }
-
-    def applyBitMask(andMask :Bits, orMask :Bits) = {
+    def applyBitMask(location :Bits, andMask :Bits, orMask :Bits) = {
         (location & andMask) | orMask
     }
 
-    def getCoreState(index :Int location :Bits = this.location) = {
+    def get(location :Bits, index :Int) = {
         val start = index * stateWidth
         val end = start + stateWidth - 1
         location(end, start)
@@ -121,26 +50,10 @@ class StatusBits (
         applyBitMask(location, andBitMask, orBitMask)
     }
 
-
-    def makeUntouched = {
-        val andBitMask = ~(UInt(1) << ((io.core << indexShift) + UInt(1)))
-        val orBitMask = UInt(1) << (io.core << indexShift)
-        location := applyBitMask(location, andBitMask, orBitMask)
-    }
-
-}
-
-object StatusBits {
-    val WRITTEN_BEFORE_START = Bits("b00")  //initial state
-    val UNTOUCHED_SINCE_START = Bits("b01")
-    val WRITTEN_AFTER_START = Bits("b11")
-    val WRITTEN_IN_COMMIT = Bits("b10")
-
-    val stateWidth = WRITTEN_BEFORE_START.getWidth
-    val indexShift = log2Up(stateWidth)
-
-    def apply(nrCores :Int, size :Int) = {
-        Module(new StatusBits(nrCores, size))
+    def makeUntouched(location :Bits, index :UInt) = {
+        val andBitMask = ~(UInt(1) << ((index << indexShift) + UInt(1)))
+        val orBitMask = UInt(1) << (index << indexShift)
+        applyBitMask(location, andBitMask, orBitMask)
     }
 
     def orMap(location :Bits, f :(Bits, Int) => Bool) = {
@@ -214,20 +127,65 @@ class TransSpm(
     }
 
     // Status bits
-    val statusBits = StatusBits(nrCores, statusBitsSize)
+    val statusBits = Vec.fill(statusBitsSize)(RegInit(StatusBits(nrCores)))
 
-    statusBits.io.addr := io.slave.M.Addr >> log2Up(granularity)
-    statusBits.io.core := io.core
+    // Each of the OR gates here has as input the left bit of a core state
+    // for every location. It is used to check whether any location is in the
+    // WRITTEN_AFTER_START state. It is ignored during commits, that is the
+    // only moment in which the state can be WRITTEN_IN_COMMIT
+    val allUntouchedArr = new Array[Bool](nrCores)
+    for (core <- 0 until nrCores) {
+        val leftBits = new Array[Bits](statusBitsSize)
+        for (addr <- 0 until statusBitsSize) {
+            val location = statusBits(addr)
+            val coreState = StatusBits.get(location, core)
+            leftBits(addr) = coreState(1)
+        }
+        allUntouchedArr(core) = orAll(leftBits) === Bits(0)
+    }
+    val allUntouched = Vec(allUntouchedArr)
+
+    // Each of these bit-strings is the concatenation of the right bit of a
+    // core state for every location. As explained below, they are used to
+    // detect the end of a commit
+    val allRightBitsArr = new Array[Bits](nrCores)
+    for (core <- 0 until nrCores) {
+        val rightBits = new Array[Bits](statusBitsSize)
+        for (addr <- 0 until statusBitsSize) {
+            val location = statusBits(addr)
+            val coreState = StatusBits.get(location, core)
+            rightBits(addr) = coreState(0)
+        }
+        allRightBitsArr(core) = catAll(rightBits)
+    }
+    val allRightBits = Vec(allRightBitsArr)
+
+    // After masking the bit for the current location, the negation of the
+    // recursive OR of the bit strings for this core tells whether all the
+    // read locations have been written to, marking the end of a commit. The
+    // masking of the current location assumes a write in that location in the
+    // current cycle.
+    val allWritten = ~((allRightBits(io.core) & ~(UInt(1) << io.slave.M.Addr))
+            .orR)
 
     // Commit bits
     val inCommit = RegInit(Bits(0, width = nrCores))
 
-    val shouldWrite = io.slave.M.Cmd === OcpCmd.WR && statusBits.io.canWrite
+    val currentStatusBits = getStatusBits(io.slave.M.Addr)
 
-    io.db := Bits(0)
-    io.location := Bits(0)
-    io.commits := Bits(0)
-    io.written := Bits(0)
+    val hasUncommitted = StatusBits.orMap(currentStatusBits, isUncommitted _)
+    val hasInCommit = StatusBits.orMap(currentStatusBits, isInCommit _)
+    val committingHere = hasUncommitted || hasInCommit
+
+    val canRead = ~hasUncommitted
+    val canWrite = ~committingHere && (inCommit(io.core) || allUntouched(io.core))
+
+    val shouldWrite = io.slave.M.Cmd === OcpCmd.WR && canWrite
+
+    io.db := hasUncommitted ## hasInCommit
+    io.location := currentStatusBits
+    io.commits := StatusBits.makeWrittenBefore(currentStatusBits, io.core)
+    io.written := allWritten
 
     val cmdReg = Reg(next = io.slave.M.Cmd)
     val dataReg = RegInit(io.slave.M.Data)
@@ -248,21 +206,18 @@ class TransSpm(
 
     switch (io.slave.M.Cmd) {
         is (OcpCmd.RD) {
-            when (statusBits.io.canRead) {
-                statusBits.io.dva := Bool(true)
-                statusBits.io.data := UNTOUCHED_SINCE_START
+            when (canRead) {
+                currentStatusBits := StatusBits.makeUntouched(currentStatusBits, io.core)
             }
         }
 
         is (OcpCmd.WR) {
-            when (statusBits.io.canWrite) {
-                statusBits.io.dva := Bool(true)
-
-                when (statusBits.io.allWritten) {
-                    statusBits.io.data := WRITTEN_BEFORE_START
-
+            when (canWrite) {
+                when (allWritten) {
+                    doCommit(io.core, io.slave.M.Addr >> log2Up(granularity))
+                    // currentStatusBits := StatusBits.write(currentStatusBits, io.core, Bool(false))
                 }.otherwise {
-                    statusBits.io.data := WRITTEN_IN_COMMIT
+                    currentStatusBits := StatusBits.write(currentStatusBits, io.core, Bool(true))
                 }
 
                 inCommit(io.core) := ~allWritten
@@ -278,12 +233,11 @@ class TransSpm(
         for (addr <- 0 until statusBitsSize) {
             val location = statusBits(addr)
 
-            when (UInt(addr) === lastWrite) {
-                currentStatusBits := StatusBits.write(currentStatusBits, io.core, Bool(false))
+            val write = StatusBits.write(location, io.core, Bool(false))
+            val commit = StatusBits.makeWrittenBefore(location, io.core)
+            val isAddr = UInt(addr) === lastWrite
 
-            }.otherwise {
-                location := StatusBits.makeWrittenBefore(currentStatusBits, io.core)
-            }
+            location := Mux(isAddr, write, commit)
         }
     }
 
@@ -369,6 +323,7 @@ class TransSpmTester(dut: TransSpm) extends Tester(dut) {
 
   // Transactional tests
 
+  // Simple failed commit on a single variable
   poke(dut.io.core, 0)
   read(0)
 
@@ -377,66 +332,122 @@ class TransSpmTester(dut: TransSpm) extends Tester(dut) {
 
   poke(dut.io.core, 0)
   expect(write(0, 0xFF), 1)
-  sys.exit(0)
 
-  // Write to the same memory location without reading in between should fail
-  expect(write(0, 0xFF), 0)
-  expect(write(0, 0xF0), 1)
-
-  // Failed writes should not affect memory
-  expect(read(0), 0xFF)
-
-  // Write to the same memory location with a read from the same core in
-  expect(write(0, 0xFF), 0)
-  expect(read(0), 0xFF)
-  expect(write(0, 0xF0), 0)
-
-  // Write to memory in the same granularity block should fail
-  if (GRANULARITY != 1) {
-      expect(write(GRANULARITY * 9, 0xFF), 0)
-      expect(write(GRANULARITY * 9 + 1, 0xF0), 1)
+  // Failed commit on more than one variable
+  poke(dut.io.core, 0)
+  for (addr <- 0 until(256, GRANULARITY)) {
+      read(addr)
   }
 
-  // Write to memory in different granularity blocks should succeed
-  expect(write(GRANULARITY * 5, 0xFF), 0)
-  expect(write(GRANULARITY * 3, 0xF0), 0)
-
-  // Write frrom a different core should invalidate subsequent writes
-  // from other cores
-  poke(dut.io.core, 0)
-  read(GRANULARITY * 8)
   poke(dut.io.core, 1)
-  read(GRANULARITY * 8)
-  expect(write(GRANULARITY * 8, 0xF0), 0)
-  poke(dut.io.core, 0)
-  expect(write(GRANULARITY * 8, 0xFF), 1)
-
-  // The first write after a read shoudl always succeed
-  poke(dut.io.core, 0)
-  read(GRANULARITY * 8)
-  poke(dut.io.core, 1)
-  read(GRANULARITY * 8)
-  expect(write(GRANULARITY * 8, 0xF0), 0)
-  poke(dut.io.core, 0)
-  expect(read(GRANULARITY * 8), 0xF0)
-  expect(write(GRANULARITY * 8, 0xFF), 0)
-
-  // Unlike CAS, LL/SC doesn't siffer the ABA problem
-  val aValue = 0x19
-  val bValue = 0x84
+  for (addr <- 0 until(256, GRANULARITY)) {
+      expect(write(addr, 0xFF), 0)
+  }
 
   poke(dut.io.core, 0)
-  read(GRANULARITY * 8)                     // So that next write doesn't fail
-  expect(write(GRANULARITY * 8, aValue), 0) // a value is written by core 0
-  expect(read(GRANULARITY * 8), aValue)     // a value is read
+  for (addr <- 0 until(256, GRANULARITY)) {
+      expect(write(addr, 0xFF), 1)
+  }
+
+  // Even writing one variable makes the whole transaction not commit
+  poke(dut.io.core, 0)
+  for (addr <- 0 until(256, GRANULARITY)) {
+      read(addr)
+  }
 
   poke(dut.io.core, 1)
-  expect(read(GRANULARITY * 8), aValue)     // So that next write doesn't fail
-  expect(write(GRANULARITY * 8, bValue), 0) // value b is written by core 1
-  expect(read(GRANULARITY * 8), bValue)     // So that next write doesn't fail
-  expect(write(GRANULARITY * 8, aValue), 0) // vaue a is written again by core 1
+  expect(write(128, 0xFF), 0)
+
   poke(dut.io.core, 0)
-  expect(write(GRANULARITY * 8, 0xFF), 1)   // Store Conditional fails
+  for (addr <- 0 until(256, GRANULARITY)) {
+      expect(write(addr, 0xFF), 1)
+  }
+
+  // Commits can not overlap
+  poke(dut.io.core, 0)
+  for (addr <- 0 until(256, GRANULARITY)) {
+      read(addr)
+  }
+
+  poke(dut.io.core, 1)
+  for (addr <- 0 until(256, GRANULARITY)) {
+      read(addr)
+  }
+
+  poke(dut.io.core, 0)
+  for (addr <- 0 until(128, GRANULARITY)) {
+      expect(write(addr, 0xFF), 0)
+  }
+
+  // Already committed by core 0
+  poke(dut.io.core, 1)
+  for (addr <- 0 until(128, GRANULARITY)) {
+      expect(write(addr, 0xFF), 1)
+  }
+
+  // Not yet committed by core 0
+  for (addr <- 128 until(256, GRANULARITY)) {
+      expect(write(addr, 0xFF), 1)
+  }
+
+  // Write to the same memory location without reading in between should fail
+  // expect(write(0, 0xFF), 0)
+  // expect(write(0, 0xF0), 1)
+  //
+  // // Failed writes should not affect memory
+  // expect(read(0), 0xFF)
+  //
+  // // Write to the same memory location with a read from the same core in
+  // expect(write(0, 0xFF), 0)
+  // expect(read(0), 0xFF)
+  // expect(write(0, 0xF0), 0)
+  //
+  // // Write to memory in the same granularity block should fail
+  // if (GRANULARITY != 1) {
+  //     expect(write(GRANULARITY * 9, 0xFF), 0)
+  //     expect(write(GRANULARITY * 9 + 1, 0xF0), 1)
+  // }
+  //
+  // // Write to memory in different granularity blocks should succeed
+  // expect(write(GRANULARITY * 5, 0xFF), 0)
+  // expect(write(GRANULARITY * 3, 0xF0), 0)
+  //
+  // // Write frrom a different core should invalidate subsequent writes
+  // // from other cores
+  // poke(dut.io.core, 0)
+  // read(GRANULARITY * 8)
+  // poke(dut.io.core, 1)
+  // read(GRANULARITY * 8)
+  // expect(write(GRANULARITY * 8, 0xF0), 0)
+  // poke(dut.io.core, 0)
+  // expect(write(GRANULARITY * 8, 0xFF), 1)
+  //
+  // // The first write after a read shoudl always succeed
+  // poke(dut.io.core, 0)
+  // read(GRANULARITY * 8)
+  // poke(dut.io.core, 1)
+  // read(GRANULARITY * 8)
+  // expect(write(GRANULARITY * 8, 0xF0), 0)
+  // poke(dut.io.core, 0)
+  // expect(read(GRANULARITY * 8), 0xF0)
+  // expect(write(GRANULARITY * 8, 0xFF), 0)
+  //
+  // // Unlike CAS, LL/SC doesn't siffer the ABA problem
+  // val aValue = 0x19
+  // val bValue = 0x84
+  //
+  // poke(dut.io.core, 0)
+  // read(GRANULARITY * 8)                     // So that next write doesn't fail
+  // expect(write(GRANULARITY * 8, aValue), 0) // a value is written by core 0
+  // expect(read(GRANULARITY * 8), aValue)     // a value is read
+  //
+  // poke(dut.io.core, 1)
+  // expect(read(GRANULARITY * 8), aValue)     // So that next write doesn't fail
+  // expect(write(GRANULARITY * 8, bValue), 0) // value b is written by core 1
+  // expect(read(GRANULARITY * 8), bValue)     // So that next write doesn't fail
+  // expect(write(GRANULARITY * 8, aValue), 0) // vaue a is written again by core 1
+  // poke(dut.io.core, 0)
+  // expect(write(GRANULARITY * 8, 0xFF), 1)   // Store Conditional fails
 }
 
 object TransSpmTester {
@@ -450,9 +461,3 @@ object TransSpmTester {
       }
   }
 }
-
-// def apply(nrCores :Int) = {
-//     val location = Bits()
-//     location := Fill(nrCores, WRITTEN_BEFORE_START)
-//     location
-// }
