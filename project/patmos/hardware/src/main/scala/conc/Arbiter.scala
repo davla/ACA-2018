4e0d3d73bdd6a5a0697111e9d0ebd17f6a8624eb
Davide Laezza <truzzialrogo@gmx.com>
2018-04-30 18:59:16 +0200

    Commented Arbiter

--- project/patmos/hardware/src/main/scala/conc/Arbiter.scala
+++ project/patmos/hardware/src/main/scala/conc/Arbiter.scala
@@ -11,19 +11,13 @@ import conc.Util._
 import ocp._
 import patmos.Constants._
 
-/**
+/*
+    This module has roughly the same feature as NodeSPM. Briefly, it performs
+    time-slotted arbitration between an OCP master and an OCP slave.
 
- Local arbitration with the TDM counter and a tiny FSM.
-
-   * Tree to the memory is node local enabled AND and an OR for merge
-   * Three state FSM: idle, read, write
-   * loose one cycle by registering the command and switching state
-   * local counter tells when slot is there, drives the and gate
-   * DVA generated from FSM, not from memory
-     * read data comes from memory one cycle after slot, dva is simply a register delay
-     * write dva also delayed to be back in idle for pipelined operation, check this out with Patmos or a test case
-   * memory DVA is ignored as we know the timing
-   * maybe test also byte and word access, should work as in any other spm
+    The additional features are: the output of the core ID together with the
+    OCP signals, when the time slots comes; and the (unused) possiility of
+    having a delay of more than one clock cycle before the slave response.
  */
 class Arbiter(
     id: Int,
@@ -31,49 +25,78 @@ class Arbiter(
     delay :Int = 1
 ) extends Module {
 
+    /*
+      slave     : the OCp slave port, to be connected to the arbitrated master
+      master    : the OCP master port, to be connected to te slave
+      cores     : the core ID, output thether with the master signals
+    */
     val io = new Bundle() {
         val slave = new OcpCoreSlavePort(ADDR_WIDTH, DATA_WIDTH)
         val master = new OcpCoreMasterPort(ADDR_WIDTH, DATA_WIDTH)
         val core = UInt(OUTPUT, log2Up(nrCores))
     }
 
+    /*
+        This wastes one bit in case there is no delay and the waitingDva state
+        is not used, but otherwise its encoding would overlap with another state.
+    */
     val waitingCmd :: waitingTurn :: waitingDva :: Nil = Enum(UInt(), 3)
     val state = RegInit(waitingCmd)
 
     // TODO: how to reset with a harmless IDLE command?
     val masterReg = RegInit(io.slave.M)
+
+    // Register for the DVA reply, NULL by default
     val dvaRepl = Reg(init = OcpResp.NULL, next = OcpResp.NULL)
 
+    // Counter for the time slot
     val cnt = RegInit(UInt(0, width = log2Up(nrCores)))
     cnt := Mux(cnt === UInt(nrCores - 1), UInt(0), cnt + UInt(1))
+
+    // True when the time-slot comes
     val enabled = cnt === UInt(id)
+
+    // True after `delay` clock cycles from the eabled signal
     val dva = cnt === UInt((id + delay) % nrCores)
 
     // Data comes from the SPM, response from the FSM
     io.slave.S.Resp := dvaRepl
     io.slave.S.Data := io.master.S.Data
 
+    // Default OCP master signals to zero to allow external OR-ing
     io.master.M.Addr := UInt(0)
     io.master.M.Data := UInt(0)
     io.master.M.Cmd := UInt(0)
     io.master.M.ByteEn := UInt(0)
 
+    // Default core ID to zero to allow external OR-ing
     io.core := UInt(0)
 
+    // Finite state machine
     switch (state) {
         is (waitingCmd) {
+            // Sampling while waiting for command
             masterReg := io.slave.M
+
+            // Waiting for turn if a command is received
             state := Mux(io.slave.M.Cmd === OcpCmd.IDLE,
                     waitingCmd, waitingTurn)
         }
 
         is (waitingTurn) {
+
+            // Only on time-slot
             when (enabled) {
+
+                // Sending commands only for this cycle
                 masterReg.Cmd := OcpCmd.IDLE
                 io.master.M := masterReg
                 io.core := UInt(id)
 
-                // Scala if, generates different hardware
+                /*
+                    At synthesis time, switching to the appropriate state
+                    depending on the delay
+                */
                 if (delay == 0) {
                     dvaRepl := OcpResp.DVA
                     state := waitingCmd
@@ -85,7 +108,11 @@ class Arbiter(
         }
 
         is (waitingDva) {
+
+            // Only when the slave data are actually valid
             when (dva) {
+
+                // Sending va to the master and back to listening for commands
                 dvaRepl := OcpResp.DVA
                 state := waitingCmd
             }
